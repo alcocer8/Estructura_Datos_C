ca237e13be4e52b2bf4d772a35c9c3ede4b27b0a
Lorenzo A <lorenzo.alcocer8@gmail.com>
2022-09-27 19:14:12 -0500

    Actualizando funciones en lista_circular.h

--- Proyecto_1/lista_circular.h
+++ Proyecto_1/lista_circular.h
@@ -1,27 +1,110 @@
+int length_lista_cicular(Nodo *list){
+	if (!list)
+		return 0;
+	
+	Nodo *aux = list;
+	int i = 0;
+	do{
+		i++;
+		aux = aux->sig;
+	}while(aux != list);
+
+	return i;
+}
+
 int insertar_lista_circular(Nodo **list){
 	Nodo *new = crear_nodo();
 
 	if (!*list)
-		new->sig = *list;
-	else{
-
+	{
+		new->sig = new;
+		*list = new;
+	}else{
 		Nodo *aux = *list;
-		while(aux->sig != *list){
-			aux = aux->sig;
-		}
-		aux->sig = new;	
+		for(; aux->sig != *list; aux = aux->sig){}		
+		new->sig = *list;
+		aux->sig = new;
 	}
 
-
-	*list = new;
-
 	printf("Dato insertado\n");
+	return 1;
+}
+
+
+int delete_lista_circular(Nodo **list){
+	if (!*list)
+	{
+		printf("La lista esta vacia.\n");
+		return 0;
+	}
+
+	
+	if ((*list)->sig == *list)
+		*list = NULL;
+	else{
+		// Busco el ultimo elemento
+		Nodo *final = *list;
+
+		do{
+			final = final->sig;
+		}while(final->sig != *list);
+
+		// Matengo la referencia
+		Nodo *inicio = *list;
+
+		// Muevo el puntero a la sig. posicion
+		*list = (*list)->sig;
+		final->sig = *list;
+
+		// Anulo y libero el valor retendio
+		inicio = NULL;
+		free(inicio);
+
+		// Anulo y libero la posicion mantenida
+		inicio = NULL;
+		free(inicio);
+	}
+	printf("Dato eliminado. \n");
+	return 1;
 }
 
 void print_lista_circular(Nodo *list){
-	for(Nodo *aux = list; aux->sig != list; aux = aux->sig){
-		printf("Dato: %i\n", aux->i);
+	if (!list)
+	{
+		printf("Lista vacia.\n");
+		return;
 	}
 
+	Nodo *aux = list;
+
+	do{
+		printf("Dato: %i\n", aux->i);
+		aux = aux->sig;
+	}while(aux != list);
+
 	printf("Fin de lista\n");
+}
+
+int sort_lista_circular(Nodo **list){
+	if (!*list)
+	{
+		printf("Es NULL\n");
+		return 0;
+	}
+
+	
+	int n = length_lista_cicular(*list);
+	int values[n];
+	Nodo *aux = *list;
+
+	for (int i = 0; i < n; ++i, aux = aux->sig)
+		values[i] = aux->i;
+	aux = *list;
+	bubble_sort(values, n);
+
+	
+	for (int i = 0; i < n; ++i, aux = aux->sig)
+		aux->i = values[i];
+
+	printf("Cola ordenada\n");
 }
--- Proyecto_1/main.c
+++ Proyecto_1/main.c
@@ -3,7 +3,7 @@
 #include "nodo.h"
 #include "lista_simple.h"
 #include "lista_doble.h"
-//#include "lista_circular.h"
+#include "lista_circular.h"
 #include "cola_simple.h"
 #include "cola_doble.h"
 #include "cola_circular.h"
@@ -11,7 +11,15 @@
 
 int main(int argc, char const *argv[])
 {
-	Cola *cola = crear_cola();
+	Nodo *list = NULL;	
+
+	insertar_lista_circular(&list);
+	insertar_lista_circular(&list);
+	insertar_lista_circular(&list);
+
+	sort_lista_circular(&list);
+	
+	print_lista_circular(list);
 
 	return 0;
 }
